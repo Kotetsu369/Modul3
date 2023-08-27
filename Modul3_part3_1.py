c888e20f148f86b96715e96529c6f3b188b82f5d
Kotetsu369 <christi-95@mail.ru>
2023-08-27 15:21:29 +0900

    modul3_part1 t changes

---
+++ Modul3_part3_1.py
@@ -0,0 +1,14 @@
+import math
+
+
+def area(a, b, c):
+    P = ((a + b + c) // 2)
+    P1 = P ** (P - a) ** (P - b) ** (P - c)
+    S = math.sqrt(P1)
+    return (S)
+
+
+a = int(input("input a"))
+b = int(input("input b"))
+c = int(input("input c"))
+print(area(a, b, c))
